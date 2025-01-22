ec14f194956a07aa366d6d930ab8ed96de4d8294
muhsin08 <mmuhsi944@gmail.com>
2025-01-22 12:50:59 +0530

    second commit

--- worker_app/urls.py
+++ worker_app/urls.py
@@ -3,5 +3,6 @@ from .views import *
 urlpatterns = [
     path('Workers_profile/',Workers_profile.as_view(),name="Workers_profile"),
     path('view_profile/', ViewProfile.as_view(), name='view_profile'),
+    path('certificate_edit/<int:id>/',certificate_edit.as_view(),name="certificate_edit")
 
 ]
--- worker_app/views.py
+++ worker_app/views.py
@@ -1,3 +1,6 @@
+from multiprocessing.pool import worker
+from django.contrib import messages
+
 from django.shortcuts import render, redirect
 from .forms import WorkerProfileForm
 from .models import WorkerProfile, LoginDetails
@@ -36,19 +39,29 @@ class Workers_profile(View):
 
 
 
-# class ViewProfile(View):
-#     def get(self, request, id):
-#         items = get_object_or_404(WorkerProfile, pk=id)
-#         # Assuming `LoginDetails` has a `user` field that is a foreign key to `WorkerProfile`
-#         my_user = LoginDetails.objects.filter(user=items)
-#         return render(request, 'workers_template/manage_skills.html', {'user': items, 'my_user': my_user})
 
 class ViewProfile(View):
     def get(self,request):
-        user=WorkerProfile.objects.all()
-        my_user=LoginDetails.objects.all()
-        return render(request,'workers_template/manage_skills.html',{'user':user ,'my_user':my_user})
+        worker_id=request.session['login_id']
+        print("asdasdadas",worker_id)
+        data=WorkerProfile.objects.filter(user=worker_id)
 
+        return render(request,'workers_template/manage_skills.html',{'user':data})
+class certificate_edit(View):
+    def get(self, request, id):
+        edit_certificate = get_object_or_404(WorkerProfile, pk=id)
+        return render(request, 'workers_template/update_skill.html', {'data': edit_certificate})
+
+    def post(self, request, id):
+        edited_certificates = get_object_or_404(WorkerProfile, pk=id)
+        edit_form = WorkerProfileForm(request.POST, request.FILES, instance=edited_certificates)
+        if edit_form.is_valid():
+            datas = edit_form.save(commit=False)
+            datas.connect = LoginDetails.objects.get(id=request.session['login_id'])
+            datas.save()
+            messages.success(request, 'document edited')
+            return redirect('view_profile')
+        return render(request,"workers_template/update_skill.html",{'edited':edited_certificates})
 
 
 
